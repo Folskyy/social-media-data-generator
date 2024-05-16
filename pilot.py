608a66de40ae6812fc2bbd5e8c2b10d719a3bb2f
Gabriel Cruz <gabrielvieirahc@gmail.com>
2024-05-16 16:33:02 -0300

    read main args

--- pilot.py
+++ pilot.py
@@ -1,29 +1,32 @@
-#%%
 import geopandas as gpd
 import pandas as pd
 import numpy as np
-# from shapely import Point
+import matplotlib.pyplot as plt
+import argparse
 
-#%%
-# leitura do número de pontos a serem inseridos
-entrada = int(input("Insira o número de pontos a ser plotado:\n> "))
+# input argument
+entrada = argparse.ArgumentParser()
+entrada.add_argument('points_number', type=int, help="Number of data points to plot.")
+args = entrada.parse_args()
 
-# cria um dataframe para armazenar os dados da latitude e longitude
+# url = 'https://geodata.ucdavis.edu/gadm/gadm4.1/shp/gadm41_BRA_shp.zip'
+# gdf = gpd.read_file(url)
+gdf = gpd.read_file('files/BRA_shp.zip')
+
+# dataframe to save latitude and longitude data
 data = pd.DataFrame(columns=['latitude', 'longitude'])
 
-#%%
-for i in range(entrada):
+for i in range(args.points_number):
     # geração de 2 números aleatórios para a longitude e latitude
     latitude, longitude = np.random.rand(2)
     
     # normalização dos números aleatórios gerados (lat=90:-90; long=180:-180)
-    latitude = 180 * (latitude - 0.5)
-    longitude = 360 * (longitude - 0.5)
+    latitude = latitude * 40 - 35
+    longitude = longitude * 40 - 70
 
     # inserção de um novo par de dados no dataframe
     data.loc[i, ['latitude', 'longitude']] = (latitude, longitude)
 
-#%%
 # conversão de um dataframe para um geodataframe
 # PARAMETROS:
 # data -> conjunto de dados para formar uma tabela
@@ -32,9 +35,14 @@ for i in range(entrada):
 # lista de Pontos (shapely.Points) e interpretá-los como um objeto geometry
 # crs -> sistema de referencia. Ele define o sistema de coordenadas utilizado
 # para representar a localização espacial de dados geográficos.
-gdf = gpd.GeoDataFrame(data, geometry=gpd.points_from_xy(data['latitude'], data['longitude'], crs="EPSG:4326"))
-#%%
+# , crs="EPSG:4326"
+# coordenadas = list(zip(data['latitude'], data['longitude']))
+
+gdf2 = gpd.GeoDataFrame(data, geometry=gpd.points_from_xy(data['longitude'], data['latitude'], crs="EPSG:4326"))
+
+ax = gdf.plot(color='white', edgecolor='black')
 # visualização
-gdf.plot()
+gdf2.plot(ax=ax, color='red')
+plt.show()
+
 gdf.head()
-# %%
