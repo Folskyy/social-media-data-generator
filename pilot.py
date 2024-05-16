fd95dadcb3faab43e730d4fc6e9a71ea758ffbae
Gabriel Cruz <gabrielvieirahc@gmail.com>
2024-05-16 16:47:13 -0300

    latitude and longitude adjustment

--- pilot.py
+++ pilot.py
@@ -10,32 +10,30 @@ argv.add_argument('points_number', type=int, help="Number of data points to plot
 args = argv.parse_args()
 
 # url = 'https://geodata.ucdavis.edu/gadm/gadm4.1/shp/gadm41_BRA_shp.zip'
-# gdf = gpd.read_file(url)
-gdf = gpd.read_file('files/BRA_shp.zip')
+# br_shapefile = gpd.read_file(url)
+br_shapefile = gpd.read_file('files/BRA_shp.zip')
 
 # dataframe to save latitude and longitude data
 data = pd.DataFrame(columns=['latitude', 'longitude'])
 
+# Brazil's geographic limitations
 min_lat, max_lat = -33.75, 5.27
 min_lon, max_lon = -73.99, -34.79
 
 for i in range(args.points_number):
     # 2 random numbers to represent longitude and latitude
-    latitude = np.random.uniform(min=min_lat, max=max_lat)
-    longitude = np.random.uniform(min=min_lon, max=max_lon)
-    
-    # # data normalization (lat=5:-33; long=180:-180)
-    # latitude = latitude * 40 - 35
-    # longitude = longitude * 40 - 70
+    latitude = np.random.uniform(min_lat, max_lat)
+    longitude = np.random.uniform(min_lon, max_lon)
 
-    # inserção de um novo par de dados no dataframe
+    # data save on the dataframe
     data.loc[i, ['latitude', 'longitude']] = (latitude, longitude)
 
-gdf2 = gpd.GeoDataFrame(data, geometry=gpd.points_from_xy(data['longitude'], data['latitude'], crs="EPSG:4326"))
+# transform the dataframe saved to shapely points objects
+geometry = gpd.points_from_xy(data['longitude'], data['latitude'], crs="EPSG:4326")
+gdf = gpd.GeoDataFrame(data, geometry=geometry)
 
-ax = gdf.plot(color='white', edgecolor='black')
-# visualização
-gdf2.plot(ax=ax, color='red')
+# graph colors setting up and brazil shapefile plot
+ax = br_shapefile.plot(color='white', edgecolor='black')
+# points plot upside the shapefile
+gdf.plot(ax=ax, color='red')
 plt.show()
-
-gdf.head()
