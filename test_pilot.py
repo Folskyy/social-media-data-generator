e87ba837106a4f1a776e2028fe083f05fb85d32e
Gabriel Cruz <gabrielvieirahc@gmail.com>
2024-06-01 06:05:25 -0300

    temporal component tested

--- test_pilot.py
+++ test_pilot.py
@@ -5,13 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import datetime
 #%%
+# dados recebidos como parametro do programa
 point_num = 10
 temporal_comp = 10 # minutes
 start_time = datetime.datetime(2024, 5, 28, 15, 43, 55)
-end_time = start_time + datetime.timedelta(minutes = temporal_comp)
+end_time = datetime.datetime(2024, 5, 28, 18, 23, 50)
 
 # Brazil shapefile download
-br_shapefile = gpd.read_file('files/BRA_shp.zip')
+br_shapefile = gpd.read_file('../files/BRA_shp.zip')
 
 # Dataframe to save latitude and longitude data
 data = pd.DataFrame(columns=['latitude', 'longitude'])
@@ -21,14 +22,26 @@ min_lat, max_lat = -33.75, 5.27
 min_lon, max_lon = -73.99, -34.79
 
 #%%
-for i in range(point_num):
-    # 2 random numbers to represent longitude and latitude
-    latitude = np.random.uniform(min_lat, max_lat)
-    longitude = np.random.uniform(min_lon, max_lon)
-    actual_time = str(start_time + datetime.timedelta(minutes=i))
+actual_time = start_time
+max_time, j = start_time, 0
 
-    # Data save on the dataframe
-    data.loc[i, ['latitude', 'longitude', 'time']] = (latitude, longitude, actual_time)
+while max_time < end_time:
+    max_time = max_time + datetime.timedelta(minutes=temporal_comp)
+    if max_time > end_time:
+            max_time = end_time
+    for i in range(point_num):
+        # 2 random numbers to represent longitude and latitude
+        latitude = np.random.uniform(min_lat, max_lat)
+        longitude = np.random.uniform(min_lon, max_lon)
+
+        time = float(np.random.uniform(actual_time.timestamp(), max_time.timestamp()))
+        time = str(datetime.datetime.fromtimestamp(time))
+
+        # Data save on the dataframe
+        data.loc[j * i + i, ['latitude', 'longitude', 'time']] = (latitude, longitude, time)
+
+    actual_time = actual_time + datetime.timedelta(minutes=temporal_comp)
+    j += 1
 
 #%%
 # Transform the dataframe saved into shapely points objects
