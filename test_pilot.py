06386adb41de3dfbce6bb9ef879d8fa954154fcb
Gabriel Cruz <gabrielvieirahc@gmail.com>
2024-06-02 17:43:05 -0300

    points in the shapefile

--- test_pilot.py
+++ test_pilot.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime
+from shapely import Point
 #%%
 # dados recebidos como parametro do programa
 point_num = 10
@@ -22,26 +23,35 @@ min_lat, max_lat = -33.75, 5.27
 min_lon, max_lon = -73.99, -34.79
 
 #%%
+def insert_points(shapefile, point_num, actual_time, max_time):
+    points = []
+    times = []
+    total_bounds = shapefile.total_bounds
+
+    while len(points) < point_num:
+        # 2 random numbers to represent longitude and latitude    
+        longitude = np.random.uniform(total_bounds[0], total_bounds[2])
+        latitude = np.random.uniform(total_bounds[1], total_bounds[3])
+        
+        random_point = Point(longitude, latitude)
+        if shapefile.contains(random_point).any():
+            print(f'point[{len(points)}] founded')
+            points.append([longitude, latitude])
+            time = np.random.uniform(actual_time.timestamp(), max_time.timestamp())
+            time = str(datetime.datetime.fromtimestamp(time))
+            times.append(time)
+        
+    return points, times
+#%%
 actual_time = start_time
-max_time, j = start_time, 0
+max_time = start_time  
 
 while max_time < end_time:
     max_time = max_time + datetime.timedelta(minutes=temporal_comp)
     if max_time > end_time:
             max_time = end_time
-    for i in range(point_num):
-        # 2 random numbers to represent longitude and latitude
-        latitude = np.random.uniform(min_lat, max_lat)
-        longitude = np.random.uniform(min_lon, max_lon)
-
-        time = float(np.random.uniform(actual_time.timestamp(), max_time.timestamp()))
-        time = str(datetime.datetime.fromtimestamp(time))
-
-        # Data save on the dataframe
-        data.loc[j * i + i, ['latitude', 'longitude', 'time']] = (latitude, longitude, time)
-
+    points, times = insert_points(br_shapefile, point_num, actual_time, max_time)
     actual_time = actual_time + datetime.timedelta(minutes=temporal_comp)
-    j += 1
 
 #%%
 # Transform the dataframe saved into shapely points objects
