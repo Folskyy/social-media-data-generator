90a55ec2a306ea49dce432ebd16887b6156109ff
Gabriel Cruz <gabrielvieirahc@gmail.com>
2024-06-14 21:34:25 -0300

    +timestamp mode +random points inside geometry(Deprecate)

--- test_pilot.py
+++ test_pilot.py
@@ -15,48 +15,52 @@ end_time = datetime.datetime(2024, 5, 28, 18, 23, 50)
 # Brazil shapefile download
 br_shapefile = gpd.read_file('../files/BRA_shp.zip')
 
-# Dataframe to save latitude and longitude data
-data = pd.DataFrame(columns=['latitude', 'longitude'])
-
 # Brazil's geographic limitations
 min_lat, max_lat = -33.75, 5.27
 min_lon, max_lon = -73.99, -34.79
 
 #%%
 def insert_points(shapefile, point_num, actual_time, max_time):
-    points = []
+    latitude_points = []
+    longitude_points = []
     times = []
     total_bounds = shapefile.total_bounds
 
-    while len(points) < point_num:
+    while len(latitude_points) < point_num:
         # 2 random numbers to represent longitude and latitude    
         longitude = np.random.uniform(total_bounds[0], total_bounds[2])
         latitude = np.random.uniform(total_bounds[1], total_bounds[3])
         
         random_point = Point(longitude, latitude)
         if shapefile.contains(random_point).any():
-            print(f'point[{len(points)}] founded')
-            points.append([longitude, latitude])
+            print(f'point[{len(latitude_points)}] founded')
+            latitude_points.append(latitude)
+            longitude_points.append(longitude)
             time = np.random.uniform(actual_time.timestamp(), max_time.timestamp())
             time = str(datetime.datetime.fromtimestamp(time))
             times.append(time)
         
-    return points, times
-#%%
+    return latitude_points, longitude_points, times
+#%% timestamp
 actual_time = start_time
 max_time = start_time  
-
+lat_points, lon_points, times = [], [], []
 while max_time < end_time:
-    max_time = max_time + datetime.timedelta(minutes=temporal_comp)
+    max_time += datetime.timedelta(minutes=temporal_comp)
     if max_time > end_time:
             max_time = end_time
-    points, times = insert_points(br_shapefile, point_num, actual_time, max_time)
+    lat, lon, time = insert_points(br_shapefile, point_num, actual_time, max_time)
+    [lat_points.append(item) for item in lat]
+    [lon_points.append(item) for item in lon]
+    [times.append(item) for item in time]
     actual_time = actual_time + datetime.timedelta(minutes=temporal_comp)
 
+#%% # no timestamp
+lat_points, lon_points, times = insert_points(br_shapefile, point_num, start_time, end_time)
 #%%
 # Transform the dataframe saved into shapely points objects
-geometry = gpd.points_from_xy(data['longitude'], data['latitude'], crs="EPSG:4326")
-gdf = gpd.GeoDataFrame(data, geometry=geometry)
+geometry = gpd.points_from_xy(lon_points, lat_points, crs="EPSG:4326")
+gdf = gpd.GeoDataFrame(pd.DataFrame({'longitude': lon_points, 'latitude': lat_points, 'time': times}), geometry=geometry)
 
 #%%
 # Graph colors setting up and Brazil shapefile plot
