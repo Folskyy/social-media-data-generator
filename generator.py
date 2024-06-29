da23fa97f79214ae3dae19e24c757e6fd754ad3d
Gabriel Cruz <gabrielvieirahc@gmail.com>
2024-06-29 00:50:43 -0300

    separate files

--- generator.py
+++ generator.py
@@ -1,45 +1,45 @@
 import geopandas as gpd
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+from utilities import insert_points
+import datetime
 
-# Input argument
 argv = argparse.ArgumentParser()
 argv.add_argument('points_number', type=int, help="Number of data points to plot.")
-argv.add_argument('shapefile', type=int, help="directory or link to a ShapeFile")
+argv.add_argument('temporal_component', type=int, help="Timestamp")
+
+argv.add_argument('shapefile', type=str, help="directory or link to a ShapeFile")
+########################################################### have to be passed by argument too
+temporal_comp = 10 # minutes
+start_time = datetime.datetime(2024, 5, 28, 15, 43, 55)
+end_time = datetime.datetime(2024, 5, 28, 18, 23, 50)
+###########################################################
 args = argv.parse_args()
 
-br_shapefile = gpd.read_file(args.shapefile)
-br_shapefile = gpd.read_file('files/BRA_shp.zip') # receber por parâmetro
+# variables definition
+points_num = args.points_number
+shapefile = gpd.read_file(args.shapefile)
 
-# Dataframe to save latitude and longitude data
-data = pd.DataFrame(columns=['latitude', 'longitude'])
+latitude, longitude, times = insert_points(shapefile, points_num, start_time, end_time)
 
-# Brazil's geographic limitations
-min_lat, max_lat = -33.75, 5.27
-min_lon, max_lon = -73.99, -34.79
-
-for i in range(args.points_number):
-    # 2 random numbers to represent longitude and latitude
-    latitude = np.random.uniform(min_lat, max_lat)
-    longitude = np.random.uniform(min_lon, max_lon)
-
-    # Data save on the dataframe
-    data.loc[i, ['latitude', 'longitude']] = (latitude, longitude)
+data = pd.DataFrame({'latitude': latitude,
+                     'longitude': longitude,
+                     'times': times
+                     })
 
 # Transform the dataframe saved into shapely points objects
 geometry = gpd.points_from_xy(data['longitude'], data['latitude'], crs="EPSG:4326")
 gdf = gpd.GeoDataFrame(data, geometry=geometry)
 
 # Graph colors setting up and Brazil shapefile plot
-ax = br_shapefile.plot(color='white', edgecolor='black')
+ax = shapefile.plot(color='white', edgecolor='black')
 # Points plot on top of the shapefile
 gdf.plot(ax=ax, color='red')
 plt.show()
 
 # Iteractive map
-m = br_shapefile.explore()
+m = shapefile.explore()
 gdf.explore(m=m, color='red')
 
 # Save all the explore() layers ploted before
