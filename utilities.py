003d5c57b1f3630c631d361f68013a0ea6977282
Gabriel Cruz <gabrielvieirahc@gmail.com>
2024-06-29 21:12:58 -0300

    intsert points using time intervals added

--- utilities.py
+++ utilities.py
@@ -1,12 +1,33 @@
-import geopandas as gpd
+import pandas as pd
 import numpy as np
 from shapely import Point
-import datetime
+from datetime import datetime, timedelta
+import sys
 
-def insert_points(shapefile, point_num, actual_time, max_time):
+def insert_points_by_time(shapefile, point_num, start_time, end_time, temporal_comp):
+    actual_time = start_time
+    delta = timedelta(minutes=temporal_comp)
+    epochs = int(((end_time - start_time).total_seconds() // 60) // temporal_comp)
+    actual_epoch = 0
+    data = pd.DataFrame(columns=['latitude','longitude','time'])
+    
+    while actual_time < end_time:
+        latitude, longitude, times = insert_points(shapefile,
+                                                   point_num,
+                                                   actual_time,
+                                                   end_time)
+        
+        sys.stdout.write(f'\n{actual_epoch}/{epochs} epochs...\n')
+        actual_epoch += 1
+        actual_time += delta
+        for i, j, k in zip(latitude, longitude, times):
+            data.loc[len(data)] = [i, j, k]
+    return data
+
+def insert_points(shapefile, point_num, start_time, end_time):
     latitude_points, longitude_points, times = [], [], []
     total_bounds = shapefile.total_bounds
-
+    
     while len(latitude_points) < point_num:
         # 2 random numbers to represent longitude and latitude    
         longitude = np.random.uniform(total_bounds[0], total_bounds[2])
@@ -14,11 +35,12 @@ def insert_points(shapefile, point_num, actual_time, max_time):
         random_point = Point(longitude, latitude)
         
         if shapefile.contains(random_point).any():
-            print(f'point[{len(latitude_points)}] founded')
+            sys.stdout.write(f'\r{len(latitude_points)}/{point_num} founded...')
             latitude_points.append(latitude)
             longitude_points.append(longitude)
-            time = np.random.uniform(actual_time.timestamp(), max_time.timestamp())
-            time = str(datetime.datetime.fromtimestamp(time))
+            time = np.random.uniform(start_time.timestamp(), end_time.timestamp())
+            time = str(datetime.fromtimestamp(time))
             times.append(time)
-        
+    
+    sys.stdout.write(f'\r{len(latitude_points)}/{point_num} founded...')
     return latitude_points, longitude_points, times
